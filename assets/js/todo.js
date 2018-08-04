d03318702b1e44b1817e8205f1cac095e18c94a9
rana11shubham <36005170+rana11shubham@users.noreply.github.com>
2018-08-04 22:57:21 +0530

    Initial Commit

---
+++ assets/js/todo.js
@@ -0,0 +1,23 @@
+$("ul").on("click","li",function(){
+     $(this).toggleClass("complete")
+ });
+
+$("ul").on("click","span",function(event){
+    
+    $(this).parent().fadeOut(1000,function(){
+    	$(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress",function(event){
+	if(event.which===13){
+		var todo=$(this).val();
+		$(this).val("");
+   $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> "+ todo +"</li>");
+   }
+});
+
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle()
+});
